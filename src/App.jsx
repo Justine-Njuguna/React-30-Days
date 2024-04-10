402401713c7d4e4d17669d415e96e20e53f8975d
Justine-Njuguna <hdenlysjustine@gmail.com>
2024-04-11 00:05:42 +0300

    configuring ESLint & React

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
+import { createRoot } from "react-dom/client";
 
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mixed",
-    }),
-  ]);
-};
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet name="Luna" animal="Dog" breed="Havanese" />
+    <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+    <Pet name="Doink" animal="Cat" breed="Mixed" />
+  </div>
+}
 
+const container = document.getElementById("root");
+const root = createRoot(container)
+root.render(<App />)
